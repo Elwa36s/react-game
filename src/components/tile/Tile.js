db4d014374b3491697991f605667a3bb3a3730cf
Elwa36s <24dds08@gmail.com>
2021-03-03 12:51:36 +0300

    Updates

--- src/components/tile/Tile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Tile.css'
-export default function Tile(props){
-return(
-    <div className={`tile tile-${props.value}`}>
-    {props.value}
-    </div>
-    )
-}
